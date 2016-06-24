9c82f2e00850fe3d481697931896e4f531271a57
Stefan Majoor <stefan@codeyellow.nl>
2016-06-24 16:20:57 +0200

    use kernelot instead kernalittle in RandomizedDensity

--- src/Alg/Algorithms/Randomized/RandomizedDensity.java
+++ src/Alg/Algorithms/Randomized/RandomizedDensity.java
@@ -41,7 +41,7 @@ public class RandomizedDensity implements FVSAlgorithmInterface
 
         // Reduce the graph already for our kernelization
         // This may reduce the k upto which we have to search by a lot
-        ReductionSolution reduced = Kernelization.kernelittle(graph, false);
+        ReductionSolution reduced = Kernelization.kernelot(graph, false);
         this.random = new Random();
 
         for (int k = 1; ;k++) {
